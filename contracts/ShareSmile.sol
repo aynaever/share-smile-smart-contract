4b73493ba1b0754599b53ea3582c7fa8f089bc22
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-13 15:38:35 +0100

    add event to smart contract of newSmile and trigger it in creation of new smile

--- contracts/ShareSmile.sol
+++ contracts/ShareSmile.sol
@@ -8,8 +8,10 @@ contract ShareSmile {
 
 	/* struct that describes what smile is */
 	struct Smile {
-		string name;
-		string message;
+		address	sender;
+		uint256	timestamp;
+		string 	name;
+		string 	message;
 	}
 
 	/* Array where smiles will be stored */
@@ -20,10 +22,14 @@ contract ShareSmile {
 		totalSmiles = 0;
 	}
 
+	/* event to acknowledge front end app of new smile */
+	event	NewSmile(address sender, uint256 timestamp, string _name, string _message);
+
 	/* Add smile to array of Smiles and increment totalSmiles by one */
 	function addSmile(string memory _name, string memory _message) public {
-		smiles.push(Smile(_name, _message));
+		smiles.push(Smile(msg.sender, block.timestamp, _name, _message));
 		totalSmiles += 1;
+		emit	NewSmile(msg.sender, block.timestamp, _name, _message);
 	}
 
 	/* Return array of smiles */
